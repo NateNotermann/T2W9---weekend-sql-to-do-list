3232ecd80095d2e577d3075922a1e0af1f3c62cd
Nate Notermann <nate.notermann@gmail.com>
2022-07-02 20:15:02 -0500

    row is red if false/uncompleted and green if true/completed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,11 @@
 $(document).ready(onReady);
 console.log('js');
 
-function onReady(){
-//click listeners here
-getTasks();  //  NOT set up yet
-$(document).on('click', '#addButton', addTask) // connects
-$(document).on('click', '#clearAllTasksButton', clearAllTasks) // connects
+function onReady() {
+    //click listeners here
+    getTasks(); //  NOT set up yet
+    $(document).on('click', '#addButton', addTask) // connects
+    $(document).on('click', '#clearAllTasksButton', clearAllTasks) // connects
 };
 
 
@@ -24,52 +24,75 @@ $(document).on('click', '#clearAllTasksButton', clearAllTasks) // connects
 
 
 // -- ADD FUNCTION -- //
-function addTask(){
-    console.log( 'function to addTask' );
+function addTask() {
+    console.log('function to addTask');
 }
 
 
 // -- BACK-END OR SERVER/ROUT/POOL FUNCTIONS -- //
 
 // -- GET FUNCTION -- //
-function getTasks(){
-    console.log( 'in getTasks (client)');
+function getTasks() {
+    console.log('START getTasks (client)');
     $.ajax({
-        url: '/tasks',
-        method: 'GET',
-    })
-    .then(function(response) {
-        console.log('GET response is:', response );
-        renderTask(response); // Send to renderTask function
-    }).catch(function (error){
-        console.log( 'Error in GET:', error );
-        alert('Error in getTask (client)')
-    });
-    console.log( 'end of getTasks (client)');
+            url: '/tasks',
+            method: 'GET',
+        })
+        .then(function (response) {
+            console.log('GET response is: (client)', response);
+            renderTask(response); // Send to renderTask function
+        }).catch(function (error) {
+            console.log('Error in GET: (client)', error);
+            alert('Error in getTask (client)')
+        });
+    console.log('END of getTasks (client)');
 
 
-} 
+}
 
-function clearAllTasks(){
-    console.log( 'function to clear all task' );
+function clearAllTasks() {
+    console.log('function to clear all task (client)');
 }
 
 // --- FRONT END OR DOM FUNCTIONS --- //
-function renderTask(tasks){
-    
-$('#tbody').empty();
-console.log('START renderTask', tasks);
-for ( let task of tasks ) {
-    console.log(task);
-    $('#tbody').append(`
-    <tr>
-    <td>${task.name}</td>
-    <td>${task.description}</td>
-    <td>${task.status}</td>
-    <td><button data-id="${task.id}">Delete</button></td>
-    </tr>
-    `);
-}
-$('input').val('');
-    console.log( 'START renderTask');
-}
+function renderTask(tasks) {
+
+    $('#tbody').empty();
+    console.log('START renderTask (client)', tasks);
+        for (let task of tasks) {
+            // console.log( 'START renderTask LOOP' );
+            let taskStatus = `${task.status}`;
+            console.log( 'task start is:', `${task.status}`);
+            console.log( 'task is:', tasks);
+            console.log( 'taskStatus is:', taskStatus);
+          
+
+            if (  `${taskStatus}` === 'false' ) { // -- if 'false' append this
+                
+                $('#tbody').append(`
+                <tr class="false">
+                <td>${task.name}</td>
+                <td>${task.description}</td>
+                <td>${task.status}</td>
+                <td><button data-id="${task.id}">Completed</button></td>
+                <td><button data-id="${task.id}">Delete</button></td>
+                </tr>
+                `);
+                console.log('task status is FALSE', task);
+        }
+        else if ( taskStatus === 'true' ){ // -- if 'true' append this
+            $('#tbody').append(`
+            <tr class="true">
+                <td>${task.name}</td>
+                <td>${task.description}</td>
+                <td>${task.status}</td>
+                <td><button data-id="${task.id}">Completed</button></td>
+                <td><button data-id="${task.id}">Delete</button></td>
+            </tr>
+        `);
+            console.log('task status is TRUE', task);
+        }
+        };
+    $('input').val('');
+    console.log('END renderTask (client)');
+};
